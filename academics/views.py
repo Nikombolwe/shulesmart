66af3c9c368b986f8e00cc3dc60273693e6bcae1
Nikombolwe <mshananikombolwe9@gmail.com>
2026-09-01 14:40:18 +0300

    Fix import error in academics views

--- academics/views.py
+++ academics/views.py
@@ -1,14 +1,32 @@
 from rest_framework import viewsets
 from rest_framework.permissions import IsAuthenticated
-from .models import ParentGuardian, Student
-from .serializers import ParentGuardianSerializer, StudentSerializer
+from .models import AcademicYear, Term, GradeLevel, Stream, Subject
+from .serializers import (
+    AcademicYearSerializer, TermSerializer, 
+    GradeLevelSerializer, StreamSerializer, SubjectSerializer
+)
 
-class ParentGuardianViewSet(viewsets.ModelViewSet):
-    queryset = ParentGuardian.objects.all()
-    serializer_class = ParentGuardianSerializer
+class AcademicYearViewSet(viewsets.ModelViewSet):
+    queryset = AcademicYear.objects.all()
+    serializer_class = AcademicYearSerializer
     permission_classes = [IsAuthenticated]
 
-class StudentViewSet(viewsets.ModelViewSet):
-    queryset = Student.objects.all()
-    serializer_class = StudentSerializer
+class TermViewSet(viewsets.ModelViewSet):
+    queryset = Term.objects.all()
+    serializer_class = TermSerializer
+    permission_classes = [IsAuthenticated]
+
+class GradeLevelViewSet(viewsets.ModelViewSet):
+    queryset = GradeLevel.objects.all()
+    serializer_class = GradeLevelSerializer
+    permission_classes = [IsAuthenticated]
+
+class StreamViewSet(viewsets.ModelViewSet):
+    queryset = Stream.objects.all()
+    serializer_class = StreamSerializer
+    permission_classes = [IsAuthenticated]
+
+class SubjectViewSet(viewsets.ModelViewSet):
+    queryset = Subject.objects.all()
+    serializer_class = SubjectSerializer
     permission_classes = [IsAuthenticated]
