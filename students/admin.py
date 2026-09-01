50c5695673e5aa4cbec7da9469bdc97455e68d2f
Nikombolwe <mshananikombolwe9@gmail.com>
2026-09-01 12:55:47 +0300

    Register Student and ParentGuardian models in Django Admin

--- students/admin.py
+++ students/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import ParentGuardian, Student
 
-# Register your models here.
+@admin.register(ParentGuardian)
+class ParentGuardianAdmin(admin.ModelAdmin):
+    list_display = ('first_name', 'last_name', 'phone_number', 'school', 'email')
+    list_filter = ('school',)
+    search_fields = ('first_name', 'last_name', 'phone_number', 'school__name')
+    ordering = ('first_name', 'last_name')
+
+@admin.register(Student)
+class StudentAdmin(admin.ModelAdmin):
+    list_display = ('admission_number', 'first_name', 'last_name', 'gender', 'grade_level', 'stream', 'school', 'status')
+    list_filter = ('status', 'gender', 'school', 'grade_level')
+    search_fields = ('admission_number', 'first_name', 'last_name', 'school__name')
+    ordering = ('admission_number',)
